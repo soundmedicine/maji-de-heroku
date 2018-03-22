3f75c975e04cd6897023442bb30ebcba539ca025
Roberto Santos <robertosantos@Robertos-MacBook-Pro.local>
2018-03-22 11:55:21 -0600

    :sparkles: updated src/main.js:sparkles:

---
+++ client/src/main.js
@@ -0,0 +1,16 @@
+let button = document.getElementById('button')
+let add = document.getElementById('append')
+
+button.addEventListener('click', (e) => {
+  add.innerText = ''
+  fetch('https://sheltered-basin-72384.herokuapp.com')
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((item) => {
+      let hele = document.createElement('h3')
+      hele.innerText = item.type
+      add.append(hele)
+    })
+  })
+  .catch((err) => console.log(err))
+})
